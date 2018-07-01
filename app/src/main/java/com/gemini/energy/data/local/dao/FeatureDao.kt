047253dbc7625ff13a6fb80a2b7f3f731dfb97a8
dragnet-sh <dragnet.sh@gmail.com>
2018-07-01 18:20:14 +0545

    add feature dao

---
+++ app/src/main/java/com/gemini/energy/data/local/dao/FeatureDao.kt
@@ -0,0 +1,14 @@
+package com.gemini.energy.data.local.dao
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import com.gemini.energy.data.local.model.FeatureLocalModel
+
+@Dao
+interface FeatureDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(feature: FeatureLocalModel)
+
+}
