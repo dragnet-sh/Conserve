0b44a8c77ee49912552331d9227718314e1e2704
dragnet-sh <dragnet.sh@gmail.com>
2018-07-13 20:39:22 +0545

    basic energy serivce skeleton

--- app/src/main/java/com/gemini/energy/service/EnergyService.kt
+++ app/src/main/java/com/gemini/energy/service/EnergyService.kt
@@ -1,6 +1,5 @@
 package com.gemini.energy.service
 
-import android.util.Log
 import com.gemini.energy.domain.Schedulers
 import com.gemini.energy.domain.entity.Computable
 import com.gemini.energy.domain.gateway.AuditGateway
@@ -13,18 +12,32 @@ class EnergyService(
         private val auditGateway: AuditGateway,
         private val computableFactory: ComputableFactory) {
 
-
     // *** This returns a Unit Flag - to signal the processing is Done or Error Out as Applicable *** //
     fun crunch(): Disposable {
 
         return auditGateway.getComputable()
                 .observeOn(schedulers.observeOn)
                 .subscribeOn(schedulers.subscribeOn)
-                .subscribeWith(object: DisposableObserver<List<Computable>>() {
+                .subscribeWith(object : DisposableObserver<List<Computable>>() {
 
-                    override fun onNext(t: List<Computable>) {
-                        Log.d(TAG, "*** ENERGY SERVICE ***")
-                        Log.d(TAG, t.toString())
+                    override fun onNext(computables: List<Computable>) {
+
+                        val auditMap = computables.associateBy { it.auditId }
+                        auditMap.forEach { auditId, computable ->
+                            auditGateway.getFeature(auditId).subscribe { preAudit ->
+                                auditGateway.getFeatureByType(computable.auditScopeId)
+                                        .subscribe { featureData ->
+
+                                            // *** This Factory should give me a stream of Computable *** //
+                                            // *** Who is going to Listen to this *** //
+                                            computableFactory.build()
+
+
+                                        }
+
+                            }
+                        }
+
 
                         // Step 1: Build all the Computable
                         computableFactory.build()
