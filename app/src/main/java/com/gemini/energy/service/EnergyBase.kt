e46cadcb64237dba8a1fc25a76e17cfa40fe01c7
dragnet-sh <dragnet.sh@gmail.com>
2018-07-13 20:37:42 +0545

    refactor the base - implement the abstract methods

--- app/src/main/java/com/gemini/energy/service/EnergyBase.kt
+++ app/src/main/java/com/gemini/energy/service/EnergyBase.kt
@@ -1,4 +1,15 @@
 package com.gemini.energy.service
 
-open class EnergyBase {
+import com.gemini.energy.domain.entity.Audit
+import com.gemini.energy.domain.entity.Feature
+import com.gemini.energy.domain.entity.Zone
+
+abstract class EnergyBase {
+
+    protected lateinit var audit: Audit
+    protected lateinit var zone: Zone
+
+    abstract fun getFeaturePreAudit(preAudit: List<Feature>): List<Feature>
+    abstract fun getFeatureAuditScope(featureData: List<Feature>): List<Feature>
+
 }
--- app/src/main/java/com/gemini/energy/service/device/Motors.kt
+++ app/src/main/java/com/gemini/energy/service/device/Motors.kt
@@ -1,4 +1,21 @@
 package com.gemini.energy.service.device
 
-class Motors {
+import com.gemini.energy.domain.entity.Feature
+import com.gemini.energy.service.EnergyBase
+import com.gemini.energy.service.IComputable
+import com.gemini.energy.service.OutgoingRows
+
+class Motors : EnergyBase(), IComputable {
+
+    override fun getFeaturePreAudit(preAudit: List<Feature>): List<Feature> {
+        return listOf()
+    }
+
+    override fun getFeatureAuditScope(featureData: List<Feature>): List<Feature> {
+        return listOf()
+    }
+
+    override fun compute(): List<List<OutgoingRows>> {
+        return listOf()
+    }
 }
