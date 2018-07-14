0761f7df42e89c4f376e699ea4aafd8ae0a798f7
dragnet-sh <dragnet.sh@gmail.com>
2018-07-14 22:31:43 +0545

    add plugload device type class stubs

---
+++ app/src/main/java/com/gemini/energy/service/device/plugload/Fryer.kt
@@ -0,0 +1,12 @@
+package com.gemini.energy.service.device.plugload
+
+import com.gemini.energy.service.IComputable
+import com.gemini.energy.service.OutgoingRows
+
+class Fryer : IComputable {
+
+    override fun compute(): List<List<OutgoingRows>> {
+        return listOf()
+    }
+
+}
---
+++ app/src/main/java/com/gemini/energy/service/device/plugload/IceMaker.kt
@@ -0,0 +1,11 @@
+package com.gemini.energy.service.device.plugload
+
+import com.gemini.energy.service.IComputable
+import com.gemini.energy.service.OutgoingRows
+
+class IceMaker : IComputable {
+
+    override fun compute(): List<List<OutgoingRows>> {
+        return listOf()
+    }
+}
