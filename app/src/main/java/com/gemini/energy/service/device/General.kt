0bedce14cd361cde81d3af6f0307dc2e3dd5c259
dragnet-sh <dragnet.sh@gmail.com>
2018-07-14 22:39:42 +0545

    add device class stub for general | hvac | motors

--- app/src/main/java/com/gemini/energy/service/device/General.kt
+++ app/src/main/java/com/gemini/energy/service/device/General.kt
@@ -3,9 +3,10 @@ package com.gemini.energy.service.device
 import com.gemini.energy.service.IComputable
 import com.gemini.energy.service.OutgoingRows
 
-class Motors : IComputable {
+class General : IComputable {
 
     override fun compute(): List<List<OutgoingRows>> {
         return listOf()
     }
+
 }
--- app/src/main/java/com/gemini/energy/service/device/Hvac.kt
+++ app/src/main/java/com/gemini/energy/service/device/Hvac.kt
@@ -3,7 +3,7 @@ package com.gemini.energy.service.device
 import com.gemini.energy.service.IComputable
 import com.gemini.energy.service.OutgoingRows
 
-class Motors : IComputable {
+class Hvac : IComputable {
 
     override fun compute(): List<List<OutgoingRows>> {
         return listOf()
