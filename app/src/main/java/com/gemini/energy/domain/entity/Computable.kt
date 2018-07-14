d918a8a8516c161ba7ee586e5d75a8ac44ebd1d6
dragnet-sh <dragnet.sh@gmail.com>
2018-07-14 20:03:30 +0545

    add energy star validater flag | energy equivalent list

--- app/src/main/java/com/gemini/energy/domain/entity/Computable.kt
+++ app/src/main/java/com/gemini/energy/domain/entity/Computable.kt
@@ -1,25 +1,48 @@
 package com.gemini.energy.domain.entity
 
 import com.gemini.energy.presentation.util.EZoneType
+import com.gemini.energy.service.IComputable
 
 data class Computable<SubType>(
+
+        /**
+         * Audit Section
+         * */
         var auditId: Int,
         var auditName: String,
+
+        /**
+         * Zone Section
+         * */
         var zoneId: Int,
         var zoneName: String,
+
+        /**
+         * Categorize Device by Type - Sub Type
+         * */
         var auditScopeId: Int,
         var auditScopeName: String,
         var auditScopeType: EZoneType?,
         var auditScopeSubType: SubType?,
+
+        /**
+         * These are the Form Data Being Collected from the User
+         * */
         var featurePreAudit: List<Feature>?,
-        var featureAuditScope: List<Feature>?) {
+        var featureAuditScope: List<Feature>?,
+
+        /**
+         * The following parameters are used for Energy Efficient Equivalent
+         * */
+        var isEnergyStar: Boolean,
+        var energyEquivalent: List<IComputable>?) {
 
     constructor(): this(
             NONE, EMPTY, NONE, EMPTY, NONE, EMPTY, null,
-            null, null, null)
+            null, null, null, false, null)
 
     constructor(auditScopeSubType: SubType) : this(NONE, EMPTY, NONE, EMPTY, NONE, EMPTY, null,
-            auditScopeSubType, null, null)
+            auditScopeSubType, null, null, false, null)
 
     companion object {
         private const val EMPTY = ""
