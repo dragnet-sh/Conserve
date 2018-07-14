e1716a4be27e97af7a3658a997e7011769b3f420
dragnet-sh <dragnet.sh@gmail.com>
2018-07-14 17:03:46 +0545

    skeleton class for computable factory

--- app/src/main/java/com/gemini/energy/service/ComputableFactory.kt
+++ app/src/main/java/com/gemini/energy/service/ComputableFactory.kt
@@ -1,45 +1,55 @@
 package com.gemini.energy.service
 
-import com.gemini.energy.domain.entity.Feature
-
-
-
-class Refrigerator : EnergyBase(), IComputable {
-    override fun featurePreAudit(auditId: Int): List<Feature> {
-        return listOf()
-    }
-
-    override fun featureType(typeId: Int, subTypeId: Int): List<Feature> {
-        return listOf()
-    }
-
-    override fun compute(): List<List<OutgoingRows>> {
-        return listOf()
-    }
-}
-
-class Motors: EnergyBase(), IComputable {
-    override fun featurePreAudit(auditId: Int): List<Feature> {
-        return listOf()
-    }
-
-    override fun featureType(typeId: Int, subTypeId: Int): List<Feature> {
-        return listOf()
-    }
-
-    override fun compute(): List<List<OutgoingRows>> {
-        return listOf()
-    }
-}
-
+import com.gemini.energy.domain.entity.Computable
+import com.gemini.energy.presentation.util.EZoneType
+import io.reactivex.Flowable
 
 // ** Computable Generator Factory gives me a list of IComputable ** //
 
 // ** Type 1 Service - Emits :: Single Row of Computable Data
 // ** Type 2 Service - Emits :: List of Computable Data
 
-class ComputableFactory {
-    fun build() : IComputable? {
-        return null
+abstract class ComputableFactory {
+    abstract fun build(computable: Computable<*>): Flowable<List<IComputable>>
+
+    companion object {
+        inline fun createFactory(computable: Computable<*>): ComputableFactory =
+                when (computable.auditScopeType as EZoneType) {
+                    EZoneType.Plugload              -> PlugloadFactory()
+                    EZoneType.HVAC                  -> HvacFactory()
+                    EZoneType.Lighting              -> LightingFactory()
+                    EZoneType.Motors                -> MotorFactory()
+                    EZoneType.Others                -> GeneralFactory()
+                }
+    }
+}
+
+class PlugloadFactory : ComputableFactory() {
+    override fun build(computable: Computable<*>): Flowable<List<IComputable>> {
+        return Flowable.just(listOf())
+    }
+}
+
+class HvacFactory : ComputableFactory() {
+    override fun build(computable: Computable<*>): Flowable<List<IComputable>> {
+        return Flowable.just(listOf())
+    }
+}
+
+class LightingFactory : ComputableFactory() {
+    override fun build(computable: Computable<*>): Flowable<List<IComputable>> {
+        return Flowable.just(listOf())
+    }
+}
+
+class MotorFactory : ComputableFactory() {
+    override fun build(computable: Computable<*>): Flowable<List<IComputable>> {
+        return Flowable.just(listOf())
+    }
+}
+
+class GeneralFactory : ComputableFactory() {
+    override fun build(computable: Computable<*>): Flowable<List<IComputable>> {
+        return Flowable.just(listOf())
     }
 }
