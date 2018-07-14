8ab69085c7863ff9d9410e849950a9ee4b0ec2ed
dragnet-sh <dragnet.sh@gmail.com>
2018-07-14 16:59:11 +0545

    add subtype as generics along with default constructor

--- app/src/main/java/com/gemini/energy/domain/entity/Computable.kt
+++ app/src/main/java/com/gemini/energy/domain/entity/Computable.kt
@@ -1,12 +1,29 @@
 package com.gemini.energy.domain.entity
 
-data class Computable(
+import com.gemini.energy.presentation.util.EZoneType
+
+data class Computable<SubType>(
         var auditId: Int,
         var auditName: String,
         var zoneId: Int,
         var zoneName: String,
         var auditScopeId: Int,
         var auditScopeName: String,
-        var auditScopeType: String,
-        var auditScopeSubType: String
-)
+        var auditScopeType: EZoneType?,
+        var auditScopeSubType: SubType?,
+        var featurePreAudit: List<Feature>?,
+        var featureAuditScope: List<Feature>?) {
+
+    constructor(): this(
+            NONE, EMPTY, NONE, EMPTY, NONE, EMPTY, null,
+            null, null, null)
+
+    constructor(auditScopeSubType: SubType) : this(NONE, EMPTY, NONE, EMPTY, NONE, EMPTY, null,
+            auditScopeSubType, null, null)
+
+    companion object {
+        private const val EMPTY = ""
+        private const val NONE = -1
+    }
+
+}
