6317df53054b01b31d4b7692c636d61a10ae168c
dragnet-sh <dragnet.sh@gmail.com>
2018-07-20 12:41:36 +0545

    implement gas utility charges | minor refactor

--- app/src/androidTest/java/com/gemini/energy/EnergyUtilityTest.kt
+++ app/src/androidTest/java/com/gemini/energy/EnergyUtilityTest.kt
@@ -2,8 +2,10 @@ package com.gemini.energy
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
+import com.gemini.energy.presentation.util.ERateKey
 import com.gemini.energy.service.Electricity
 import com.gemini.energy.service.EnergyUtility
+import com.gemini.energy.service.Gas
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -12,7 +14,7 @@ import org.junit.runner.RunWith
 class EnergyUtilityTest {
 
     @Test
-    fun readUtilityData() {
+    fun readUtilityDataElectric() {
         val context = InstrumentationRegistry.getTargetContext()
         val utility = EnergyUtility(context, Electricity("A-1 TOU"))
                 .build()
@@ -27,8 +29,24 @@ class EnergyUtilityTest {
         assertEquals(listOf("0.26298", "0.23020"), structure["summer-on-peak"])
     }
 
-    companion object {
-        private const val TAG = "EnergyUtilityTest"
+    @Test
+    fun readUtilityDataGas() {
+        val context = InstrumentationRegistry.getTargetContext()
+        val utility = EnergyUtility(context, Gas())
+                .build()
+
+        val structure = utility.structure
+
+        assertEquals(8, structure.count())
+        assertEquals(listOf("0.27048"), structure[ERateKey.Slab1.value])
+        assertEquals(listOf("0.52106"), structure[ERateKey.Slab2.value])
+        assertEquals(listOf("0.95482"), structure[ERateKey.Slab3.value])
+        assertEquals(listOf("1.66489"), structure[ERateKey.Slab4.value])
+        assertEquals(listOf("2.14936"), structure[ERateKey.Slab5.value])
+        assertEquals(listOf("0.91132"), structure[ERateKey.SummerTransport.value])
+        assertEquals(listOf("1.0215"), structure[ERateKey.WinterTransport.value])
+        assertEquals(listOf("0.04672"), structure[ERateKey.Surcharge.value])
+
     }
 
 }
--- app/src/main/java/com/gemini/energy/service/EnergyUtility.kt
+++ app/src/main/java/com/gemini/energy/service/EnergyUtility.kt
@@ -1,6 +1,7 @@
 package com.gemini.energy.service
 
 import android.content.Context
+import com.gemini.energy.presentation.util.ERateKey
 
 open class EnergyUtility(private val context: Context, private val utility: IUtility) {
 
@@ -24,12 +25,15 @@ open class EnergyUtility(private val context: Context, private val utility: IUti
         val outgoing: HashMap<String, List<String>> = hashMapOf()
 
         if (collection.count() > 0) {
+            val header = collection[0]
+            collection.drop(0)
             collection.forEach {
-
                 if (it.matches(getRowIdentifier())) {
                     val result = parseLine(it, getSeparator())
-                    val key = utility.getKey(result)
-                    outgoing[key] = utility.getValue(result)
+                    utility.getKey(result)
+                            .forEachIndexed { index, key ->
+                                outgoing[key] = utility.getValue(result, header)[index]
+                            }
                 }
             }
         }
@@ -40,6 +44,7 @@ open class EnergyUtility(private val context: Context, private val utility: IUti
 
 
     companion object {
+
         private fun parseLine(line: String, separator: Char) : List<String> {
             val result = mutableListOf<String>()
             val builder = StringBuilder()
@@ -63,35 +68,61 @@ open class EnergyUtility(private val context: Context, private val utility: IUti
 
             return result
         }
+
     }
 
 }
 
 
 interface IUtility {
-    fun getKey(columns: List<String>): String
-    fun getValue(columns: List<String>): List<String>
+    fun getKey(columns: List<String>): List<String>
+    fun getValue(columns: List<String>, header: String): List<List<String>>
+
     fun getResourcePath(): String
     fun getSeparator(): Char
     fun getRowIdentifier(): Regex
     fun getRate(): String
 }
 
-//class Gas : IUtility {
-//    override fun getKey(columns: List<String>): String { return "" }
-//    override fun getValue(columns: List<String>): List<String> { return listOf() }
-//}
-
 class Electricity(private val rateStructure: String) : IUtility {
 
     enum class EKey(val index: Int) { Season(1), Peak(4) }
     enum class EValue(val index: Int) { EnergyCharge(5), Average(6) }
 
-    override fun getKey(columns: List<String>) = columns[EKey.Season.index] + "-" + columns[EKey.Peak.index]
-    override fun getValue(columns: List<String>) = listOf(columns[EValue.EnergyCharge.index], columns[EValue.Average.index])
+    override fun getKey(columns: List<String>) = listOf(columns[EKey.Season.index] + "-" + columns[EKey.Peak.index])
+    override fun getValue(columns: List<String>, header: String) = listOf(listOf(columns[EValue.EnergyCharge.index],
+            columns[EValue.Average.index]))
+
     override fun getResourcePath() = "utility/pge_electric.csv"
     override fun getSeparator(): Char = ','
     override fun getRate() = rateStructure
     override fun getRowIdentifier() = "^${getRate()}${getSeparator()}.*".toRegex()
+
+}
+
+class Gas(private val rateStructure: String = "") : IUtility {
+
+    override fun getKey(columns: List<String>) = keys
+    override fun getValue(columns: List<String>, header: String): List<List<String>> {
+        val outgoing: MutableList<List<String>> = mutableListOf()
+        val lookup = header.split(getSeparator())
+        keys.forEach {
+            outgoing.add(listOf(columns[lookup.indexOf(it)]))
+        }
+
+        return outgoing
+    }
+
+    override fun getResourcePath() = "utility/pge_gas.csv"
+    override fun getSeparator() = ','
+    override fun getRowIdentifier(): Regex { return ".*".toRegex() }
+    override fun getRate() = rateStructure
+
+    companion object {
+        private val keys = listOf(
+                ERateKey.Slab1.value, ERateKey.Slab2.value, ERateKey.Slab3.value,
+                ERateKey.Slab4.value, ERateKey.Slab5.value, ERateKey.Surcharge.value,
+                ERateKey.SummerTransport.value, ERateKey.WinterTransport.value)
+    }
 
 }
