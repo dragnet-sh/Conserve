6f40776fea858d8773899fe34bcf16f13cce3e63
dragnet-sh <dragnet.sh@gmail.com>
2018-08-01 23:11:26 +0545

    implement error handler

--- app/src/main/java/com/gemini/energy/service/device/EBase.kt
+++ app/src/main/java/com/gemini/energy/service/device/EBase.kt
@@ -10,6 +10,9 @@ import com.gemini.energy.service.*
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.functions.BiFunction
+import io.reactivex.functions.Function
 import okhttp3.OkHttpClient
 import org.json.JSONObject
 import retrofit2.Retrofit
@@ -75,7 +78,8 @@ abstract class EBase(private val computable: Computable<*>,
                             outgoingRows.dataHolder.add(it)
                         }
 
-                    }, {}, {
+                    }, { emitter.onError(it) }, {
+
                         Log.d(TAG, "Concat Operation - PRE | POST - [ON COMPLETE] - Save Data - (${thread()})")
 
                         // **** For Each Computable - Once the Collector finishes collecting - Writes that result **** //
@@ -144,69 +148,55 @@ abstract class EBase(private val computable: Computable<*>,
      * */
     private fun calculateEnergyPostState(extra: (param: String) -> Unit): Observable<DataHolder> {
 
-        return Observable.create<DataHolder> { emitter ->
+        class DataHolderMapper : Function<JsonArray, DataHolder> {
 
-            Log.d(TAG, "%^%^% Post-State Energy Calculation - (${thread()}) %^%^%")
-            if (efficientLookup()) {
-                starValidator(queryEnergyStar())
-                        .observeOn(schedulers.subscribeOn)
-                        .subscribe { starValidationFail ->
+            override fun apply(response: JsonArray): DataHolder {
 
-                            if (starValidationFail) {
+                Log.d(TAG, "### Efficient Alternate Count - [${response.count()}] - ###")
 
-                                Log.d(TAG, "### Debug - Query Filter - (${thread()})###")
-                                Log.d(TAG, queryFilter())
+                val jsonElements = response.map { it.asJsonObject.get("data") }
+                computable.efficientAlternative = jsonElements
 
-                                efficientAlternative(queryFilter())
-                                        .observeOn(schedulers.subscribeOn)
-                                        .subscribe { response ->
+                val dataHolderPostState = initDataHolder()
 
-                                            Log.d(TAG, "### Efficient Alternate Count - [${response.count()}] - ###")
+                jsonElements.forEach { element ->
+                    val postRow = mutableMapOf<String, String>()
+                    postStateFields().forEach { key ->
+                        val value = element.asJsonObject.get(key)
+                        postRow[key] = value.asString
+                    }
 
-                                            val jsonElements = response.map { it.asJsonObject.get("data") }
-                                            computable.efficientAlternative = jsonElements
+                    val postDailyEnergyUsed = element.asJsonObject.get("daily_energy_use").asDouble
+                    val cost = cost(postDailyEnergyUsed)
+                    postRow["__electric_cost"] = cost.toString()
+                    dataHolderPostState.rows?.add(postRow)
+                }
 
-                                            /**
-                                             * Cost Function to be Called - for each of the Data Row!!
-                                             * */
-                                            val dataHolderPostState = DataHolder()
-                                            dataHolderPostState.header = postStateFields()
-                                            dataHolderPostState.header?.add("__electric_cost")
+                Log.d(TAG, "## Data Holder - POST STATE  - (${thread()}) ##")
+                Log.d(TAG, dataHolderPostState.toString())
 
-                                            dataHolderPostState.computable = computable
-                                            dataHolderPostState.fileName = "${Date().time}_post_state.csv"
+                return dataHolderPostState
+            }
 
-                                            jsonElements.forEach { element ->
-                                                val postRow = mutableMapOf<String, String>()
-                                                postStateFields().forEach { key ->
-                                                    val value = element.asJsonObject.get(key)
-                                                    postRow[key] = value.asString
-                                                }
+            private fun initDataHolder(): DataHolder {
+                val dataHolderPostState = DataHolder()
+                dataHolderPostState.header = postStateFields()
+                dataHolderPostState.header?.add("__electric_cost")
 
-                                                val postDailyEnergyUsed = element.asJsonObject.get("daily_energy_use").asDouble
-                                                val cost = cost(postDailyEnergyUsed)
-                                                postRow["__electric_cost"] = cost.toString()
-                                                dataHolderPostState.rows?.add(postRow)
-                                            }
+                dataHolderPostState.computable = computable
+                dataHolderPostState.fileName = "${Date().time}_post_state.csv"
 
-                                            Log.d(TAG, "## Data Holder - POST STATE  - (${thread()}) ##")
-                                            Log.d(TAG, dataHolderPostState.toString())
-
-                                            emitter.onNext(dataHolderPostState)
-                                            emitter.onComplete()
-                                        }
-
-                            } else {
-                                computable.isEnergyStar = true
-                                emitter.onComplete()
-                            }
-
-                            extra("Post [On Complete] - Run Away Thread.")
-
-                        }
+                return dataHolderPostState
             }
 
         }
+
+        return Observable.zip(
+                starValidator(queryEnergyStar()),
+                efficientAlternative(queryEfficientFilter()),
+                BiFunction<Boolean, JsonArray, DataHolder> { validator, response ->
+                    DataHolderMapper().apply(response)
+                })
 
     }
 
@@ -215,7 +205,7 @@ abstract class EBase(private val computable: Computable<*>,
         private const val RATE = "A-1 TOU"
     }
 
-    abstract fun queryFilter(): String
+    abstract fun queryEfficientFilter(): String
     abstract fun efficientLookup(): Boolean
 
     abstract fun preAuditFields(): MutableList<String>
@@ -248,16 +238,9 @@ abstract class EBase(private val computable: Computable<*>,
     }
 
     private fun efficientAlternative(query: String): Observable<JsonArray> {
-        return Observable.create<JsonArray> {
-            parseAPIService.fetchPlugload(query)
-                    .subscribeOn(schedulers.subscribeOn)
-                    .observeOn(schedulers.observeOn)
-                    .subscribe { response ->
-                        val rows = response.getAsJsonArray("results")
-                        it.onNext(rows)
-                        it.onComplete()
-                    }
-        }
+        return parseAPIService.fetchPlugload(query)
+                .map { it.getAsJsonArray("results") }
+                .toObservable()
     }
 
 
@@ -318,7 +301,7 @@ abstract class EBase(private val computable: Computable<*>,
 
         interface ParseAPIService {
             @GET("classes/PlugLoad")
-            fun fetchPlugload(@Query("where") where: String): Observable<JsonObject>
+            fun fetchPlugload(@Query("where") where: String): Single<JsonObject>
         }
 
         companion object {
