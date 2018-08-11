05891046f3e9485adbf3948911156f367cdddcc5
dragnet-sh <dragnet.sh@gmail.com>
2018-08-11 20:46:22 +0545

    add usage type data model - tou and non tou

---
+++ app/src/main/java/com/gemini/energy/service/usage/UsageDataModel.kt
@@ -0,0 +1,29 @@
+package com.gemini.energy.service.usage
+
+interface IUsageType
+
+data class TOU(
+        var summerOn: Double,
+        var summerPart: Double,
+        var summerOff: Double,
+        var winterPart: Double,
+        var winterOff: Double
+) : IUsageType {
+    override fun toString(): String {
+        return ">>> Summer On : $summerOn \n" +
+                ">>> Summer Part : $summerPart \n" +
+                ">>> Summer Off : $summerOff \n" +
+                ">>> Winter Part : $winterPart \n" +
+                ">>> Winter Off : $winterOff"
+    }
+}
+
+data class TOUNone(
+        var summerNone: Double,
+        var winterNone: Double
+) : IUsageType {
+    override fun toString(): String {
+        return ">>> Summer None : $summerNone \n" +
+                ">>> Winter None : $winterNone \n"
+    }
+}
