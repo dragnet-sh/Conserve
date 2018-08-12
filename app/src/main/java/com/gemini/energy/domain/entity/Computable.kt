10d45d361845aac96d0680cb0ec2d783ce5ef1e7
dragnet-sh <dragnet.sh@gmail.com>
2018-08-12 22:02:11 +0545

    adding efficient alternative as a json element

--- app/src/main/java/com/gemini/energy/domain/entity/Computable.kt
+++ app/src/main/java/com/gemini/energy/domain/entity/Computable.kt
@@ -41,6 +41,7 @@ data class Computable<SubType>(
         var energyPreState: Map<String, String>?,
         var energyPostState: MutableList<Map<String, String>>?,
         var energyPostStateLeastCost: List<Map<String, String>>,
+        var efficientAlternative: JsonElement?,
         var laborCost: Double,
 
         /**
@@ -51,11 +52,11 @@ data class Computable<SubType>(
     constructor(): this(
             NONE, EMPTY, NONE, EMPTY, NONE, EMPTY, null,
             null, null, null, false,
-            null, mutableListOf(), mutableListOf(), 0.0, null)
+            null, mutableListOf(), mutableListOf(), null, 0.0, null)
 
     constructor(auditScopeSubType: SubType) : this(NONE, EMPTY, NONE, EMPTY, NONE, EMPTY,
             null, auditScopeSubType, null, null,
-            false, null, mutableListOf(), mutableListOf(), 0.0, null)
+            false, null, mutableListOf(), mutableListOf(), null, 0.0, null)
 
 
     fun mappedFeatureAuditScope(): HashMap<String, Any> = featureMapper(featureAuditScope)
