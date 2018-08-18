2f85cc74ac4c244b2c90ed422f41ea110e38bc6f
dragnet-sh <dragnet.sh@gmail.com>
2018-08-18 22:10:07 +0545

    create lighting specific usage

---
+++ app/src/main/java/com/gemini/energy/service/type/UsageLighting.kt
@@ -0,0 +1,19 @@
+package com.gemini.energy.service.type
+
+class UsageLighting : UsageHours() {
+
+    var peakHours = 0.0
+    var partPeakHours = 0.0
+    var offPeakHours = 0.0
+
+    override fun timeOfUse(): TOU {
+        return TOU(peakHours, partPeakHours, offPeakHours)
+    }
+
+    override fun nonTimeOfUse(): TOUNone {
+        return TOUNone(offPeakHours)
+    }
+
+    override fun yearly() = peakHours + partPeakHours + offPeakHours
+
+}
