51d6c00ee2f735134080e31640848c453699770c
dragnet-sh <dragnet.sh@gmail.com>
2018-09-27 18:18:45 +0545

    add color to item selected

--- app/src/main/java/com/gemini/energy/presentation/audit/detail/zone/list/adapter/ZoneListAdapter.kt
+++ app/src/main/java/com/gemini/energy/presentation/audit/detail/zone/list/adapter/ZoneListAdapter.kt
@@ -2,6 +2,7 @@ package com.gemini.energy.presentation.audit.detail.zone.list.adapter
 
 import android.app.Activity
 import android.databinding.DataBindingUtil
+import android.graphics.Color
 import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -12,6 +13,7 @@ import com.gemini.energy.R
 import com.gemini.energy.databinding.FragmentZoneListItemBinding
 import com.gemini.energy.presentation.audit.detail.zone.list.model.ZoneModel
 import com.gemini.energy.presentation.type.TypeActivity
+import timber.log.Timber
 
 class ZoneListAdapter(private val items: List<ZoneModel>, private val callbacks: OnZoneClickListener? = null,
                       private val activity: Activity):
@@ -35,6 +37,8 @@ class ZoneListAdapter(private val items: List<ZoneModel>, private val callbacks:
         return items.size
     }
 
+    private var currentPosition = RecyclerView.NO_POSITION
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.binding.zone = items[position]
         holder.binding.executePendingBindings()
@@ -48,8 +52,15 @@ class ZoneListAdapter(private val items: List<ZoneModel>, private val callbacks:
             holder.binding.showEdit = false
         }
 
+        when (currentPosition == position) {
+            true    -> holder.cardViewZone?.setBackgroundColor(Color.LTGRAY)
+            false   -> holder.cardViewZone?.setBackgroundColor(Color.WHITE)
+        }
+
         holder.cardViewZone?.setOnClickListener {
             callbacks?.onZoneClick(it, items[position], position)
+            currentPosition = position
+            notifyDataSetChanged()
         }
 
         holder.deleteImageButton?.setOnClickListener {
